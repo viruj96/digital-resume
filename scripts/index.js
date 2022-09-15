252aa7abaec99044fc16ce46870772820c500a1b
Viruj <virujbala.96@gmail.com>
2022-09-15 21:34:52 +0400

    separated js and html

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add('show');
+		} else {
+			entry.target.classList.remove('show');
+		}
+	})
+});
+
+const sections = document.querySelectorAll('.hidden');
+sections.forEach((el) => observer.observe(el));
+
+const IT_EXP = new Date().getFullYear() - 2021;
+let expYears = IT_EXP > 1 ? `${IT_EXP} years` : `${IT_EXP} year`;
+
+document.getElementById('it').innerHTML = expYears;
+
+const theme = document.getElementById('theme-toggle');
+theme.addEventListener('click', () => {
+	theme.classList.toggle('fa-moon');
+	theme.classList.toggle('fa-sun');
+
+	if (theme.classList.contains('fa-moon')) {
+		document.body.classList.add('light');
+		theme.setAttribute('title', 'Dark mode');
+		theme.style.padding = '0.5% 0.7%';
+	}
+	else {
+		document.body.classList.remove('light');
+		theme.setAttribute('title', 'Light mode');
+		theme.style.padding = '0.5%';
+	}
+});
