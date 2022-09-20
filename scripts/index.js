103972b71157bedd289fb35ae6128faf650b83ed
Viruj <virujbala.96@gmail.com>
2022-09-20 23:20:08 +0400

    separated theme toggle script

--- scripts/index.js
+++ scripts/index.js
@@ -15,20 +15,3 @@ const IT_EXP = Math.round(10 * (new Date().getTime() - new Date('2021-04').getTi
 let expYears = IT_EXP === 1 ? `${IT_EXP} year` : `${IT_EXP} years`;
 
 document.getElementById('it').innerHTML = expYears;
-
-const theme = document.getElementById('theme-toggle');
-theme.addEventListener('click', () => {
-	theme.classList.toggle('fa-moon');
-	theme.classList.toggle('fa-sun');
-
-	if (theme.classList.contains('fa-moon')) {
-		document.body.classList.add('light');
-		theme.setAttribute('title', 'Dark mode');
-		theme.style.padding = '0.5% 0.7%';
-	}
-	else {
-		document.body.classList.remove('light');
-		theme.setAttribute('title', 'Light mode');
-		theme.style.padding = '0.5%';
-	}
-});
