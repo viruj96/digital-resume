fbf804772b42190d31fc97dd045b4c222698f211
Viruj <virujbala.96@gmail.com>
2022-09-20 23:11:52 +0400

    fixed date

--- scripts/index.js
+++ scripts/index.js
@@ -11,8 +11,8 @@ const observer = new IntersectionObserver((entries) => {
 const sections = document.querySelectorAll('.hidden');
 sections.forEach((el) => observer.observe(el));
 
-const IT_EXP = new Date().getFullYear() - 2021;
-let expYears = IT_EXP > 1 ? `${IT_EXP} years` : `${IT_EXP} year`;
+const IT_EXP = Math.round(10 * (new Date().getTime() - new Date('2021-04').getTime()) / (1000 * 3600 * 24 * 365)) / 10;
+let expYears = IT_EXP === 1 ? `${IT_EXP} year` : `${IT_EXP} years`;
 
 document.getElementById('it').innerHTML = expYears;
 
