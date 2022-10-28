dfc62f3987544a4aa43cb7c8246d5155f954cc59
Viruj <virujbala.96@gmail.com>
2022-10-28 22:01:25 +0400

    collapsible items

--- scripts/index.js
+++ scripts/index.js
@@ -11,7 +11,24 @@ const observer = new IntersectionObserver((entries) => {
 const sections = document.querySelectorAll('.hidden');
 sections.forEach((el) => observer.observe(el));
 
-const IT_EXP = Math.round(10 * (new Date().getTime() - new Date('2021-04').getTime()) / (1000 * 3600 * 24 * 365)) / 10;
-let expYears = IT_EXP === 1 ? `${IT_EXP} year` : `${IT_EXP} years`;
+const itExp = Math.round(10 * (new Date().getTime() - new Date('2021-04').getTime()) / (1000 * 3600 * 24 * 365)) / 10;
 
-document.getElementById('it').innerHTML = expYears;
+document.getElementById('it').innerHTML = itExp === 1 ? `${itExp} year` : `${itExp} years`;
+
+const collapsible = document.getElementsByClassName('collapsible');
+for (let element of collapsible) {
+	element.addEventListener('click', () => {
+		let content = element.nextElementSibling;
+		content.classList.toggle('content');
+		let icon = element.querySelector('i');
+		icon.classList.toggle('fa-angles-up');
+		icon.classList.toggle('fa-angles-down');
+	});
+}
+
+const projects = document.getElementsByClassName('card--project');
+for (let project of projects) {
+	project.addEventListener('click', () => {
+		project.querySelector('a').click();
+	});
+}
