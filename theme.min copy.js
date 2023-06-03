0ae5bd5dae7b98bf90ac267780855976eda8f74a
Viruj <virujbala.96@gmail.com>
2023-06-04 02:29:35 +0400

    revert to last working commit

--- theme.min copy.js
+++
@@ -1,1 +0,0 @@
-var a=document.getElementById('theme-toggle');a.addEventListener('click',()=>{a.classList.toggle('fa-moon');a.classList.toggle('fa-sun');a.classList.contains('fa-moon')?(document.body.classList.add('light'),a.setAttribute('title','Dark mode'),a.style.padding='0.5% 0.7%'):(document.body.classList.remove('light'),a.setAttribute('title','Light mode'),a.style.padding='0.5%')});
