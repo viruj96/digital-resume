f58b409818e31e8e032e9d70f06822fc01275ba4
Viruj <virujbala.96@gmail.com>
2023-06-04 10:20:51 +0400

    set git credentials

--- minify.sh
+++ minify.sh
@@ -38,6 +38,8 @@ awk -v line="$theme_line" -v text="$theme_text" 'NR == line {$0 = text} {print}'
 minify $dest > $temp_file \
 	&& mv "$temp_file" "$dest"
 
+git config --global user.email "virujbala.96@gmail.com"
+git config --global user.name "Viruj"
 git add .
 git commit -m "Upload dist folder"
 git push -u origin main
